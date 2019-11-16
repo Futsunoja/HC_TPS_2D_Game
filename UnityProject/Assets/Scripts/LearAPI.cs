1ea96685eb719df7615aec17b610df0bf51f88ef
Futsunoja <neiy0326@gmail.com>
2019-11-16 17:01:13 +0800

    認識API

---
+++ UnityProject/Assets/Scripts/LearAPI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class LearAPI : MonoBehaviour
+{
+    public Transform transformA;
+    public Transform transformB;
+    public Camera Camera;
+
+    private void Start()
+    {
+        // 使用靜態成員 - 屬性
+        print("隨機:" + Random.value);
+
+        // 使用靜態成員 - 屬性  數學類別.PI (Mathf)
+        print("PI:" + Mathf.PI);
+
+        // 使用靜態成員 - 方法
+        print("隨機範圍:" + Random.Range(1, 10));
+        print("絕對值:" + Mathf.Abs(-99));
+
+        // 非靜態成員
+        // 需要先給予欄位存放實體物件
+        print("物件A的座標:" + transformA.position);
+
+        transformB.position = new Vector3(-2, 0, 0);
+        print("物件B的座標:" + transformB.position);
+
+        print("攝影機位置:" + Camera.depth);
+    }
+
+    private void Update()
+    {
+        // 非靜態成員 - 方法
+        transformB.Rotate(0, 0, 10);
+    }
+}
---
+++ UnityProject/Assets/Scripts/LearIF.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class LearIF : MonoBehaviour
+{
+    public bool test;
+    public string prop;
+
+    [Header("剩餘HP"),Range(0,100)]
+    public int hp;
+
+    // 更新事件:一秒執行60次
+    private void Update()
+    {
+        #region 練習if
+        // 判斷式 if 語法
+        // if (布林植){陳述式或演算法}
+        // () 裡面的布林植 true 才會執行{}
+        if (test)
+        {
+            print("打開開關");
+        }
+        // ()裡面的布林值為false才會執行else{}
+        else
+        {
+            print("關閉開關");
+        }
+
+        if (prop == "紅色藥水")
+        {
+            print("補血");
+        }
+        else if (prop == "藍色藥水")
+        {
+            print("補魔");
+        }
+        else if (prop == "黃色藥水")
+        {
+            print("補體力");
+        }
+        else
+        {
+            print("無事發生");
+        }
+        #endregion
+
+        if (70 <= hp)
+        {
+            print("安全");
+        }
+        else if (50 <= hp)
+        {
+            print("警告");
+        }
+        else if (20 <= hp)
+        {
+            print("危險");
+        }
+        else
+        {
+            print("快死了");
+        }
+    }
+}
