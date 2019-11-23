9aa90826d5dc4120b8910c971ebdf729fba4b22b
Futsunoja <57282808+Futsunoja@users.noreply.github.com>
2019-11-23 16:59:53 +0800

    水管碰撞設定

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -13,6 +13,8 @@ public class Bird : MonoBehaviour
     [Header("剛體")]
     public Rigidbody2D r2d;
 
+    public GameObject goScore, goGM;
+
     private void Start()
     {
         print(bird.rotation);
@@ -23,15 +25,25 @@ public class Bird : MonoBehaviour
     /// </summary>
     private void Jump()
     {
+        //if (dead == true)
+        //{
+        //    return;
+        //}
+
+        //如果 死亡 跳出此程式區塊
+        if (dead) return; //簡寫
+
         // 如果 玩家 按下 左鍵
         // 輸入.按下左鍵(按鍵列舉.滑鼠左鍵)(手機觸控)
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             print("玩家按下左鍵");
             r2d.Sleep();
-            r2d.gravityScale = 1; //小雞剛體.重力=1;
+            r2d.gravityScale = 1;               //小雞剛體.重力=1;
             r2d.AddForce(new Vector2(0, jump)); // 小雞剛體.增加推力(二維向量(左右,上下));
             // 小雞 往上跳
+            goScore.SetActive(true);            // 分數 顯示
+            goGM.SetActive(true);               // GM 顯示
         }
         // Rigidbody2D.SetRotation(float) 設定角度(角度)
         // Rigidbody2D.velocity 加速度（二維向量　ｘ,ｙ）
@@ -43,9 +55,26 @@ public class Bird : MonoBehaviour
     /// </summary>
     private void Dead()
     {
-
+        print("死亡");
+        dead = true;
     }
     // 固定幀數 0.002 一幀：要控制物理請寫在此事件內
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // 事件：碰撞開始–碰撞開始實執行一次（collision2D hit）存放碰到物件的資訊
+    private void OnCollisionEnter2D(Collision2D hit)
+    {
+        //碰到物品.遊戲物件.名稱
+        print(hit.gameObject.name);
+
+        if(hit.gameObject.name == "地板")
+        {
+            Dead();
+        }
+    }
     private void Update()
     {
         Jump();
